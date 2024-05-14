f3ec62c66b16f3912d83e44165cbbf290ca80055
phandongoclinh <s3881557@rmit.edu.vn>
2024-05-14 11:56:26 +0700

    FastAPI updated with tags

--- main.py
+++ main.py
@@ -1,22 +1,45 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+
 app = FastAPI()
 
 
 class Item(BaseModel):
-    # name: str
-    # price: float
-    # ready: int
     username: str
     password: str
 
 
-@app.get("/")
-async def home():
+@app.get("/", tags=["Root"])
+async def root():
     return "Hello World"
 
 
-@app.post("/login")
+@app.get("/user", tags=["user"])
+async def user():
+    return "User database"
+
+
+@app.post("/register", tags=["register"])
+async def register():
+    return "Register"
+
+
+@app.post("/login", tags=["login"])
 async def login(item: Item):
     return item
+
+
+@app.post("/dashboard", tags=["dashboard"])
+async def dashboard():
+    return "dashboard"
+
+
+@app.get("/report", tags=["report"])
+async def report():
+    return "report"
+
+
+@app.get("/camera", tags=["camera"])
+async def camera():
+    return "camera"
