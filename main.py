98d45bedba7c0f555ab4de446cb3dd545f510053
phandongoclinh <s3881557@rmit.edu.vn>
2024-05-14 10:48:26 +0700

    FastAPI

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-import cv2 as cv
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
 
 
+class Item(BaseModel):
+    # name: str
+    # price: float
+    # ready: int
+    username: str
+    password: str
+
+
+@app.get("/")
+async def home():
+    return "Hello World"
+
+
+@app.post("/login")
+async def login(item: Item):
+    return item
