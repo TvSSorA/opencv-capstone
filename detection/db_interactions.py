888e5ba5b8300ed40ea6f284f8b8bdc91f6368ca
Truong Adam Nhat Anh <92968350+adamtruong@users.noreply.github.com>
2024-08-07 21:04:08 +0700

    update to remove image collection

--- detection/db_interactions.py
+++ detection/db_interactions.py
@@ -1,4 +1,3 @@
-# db_interactions.py
 import os
 from pymongo import MongoClient
 from datetime import datetime
@@ -7,7 +6,7 @@ from .utils import logger
 
 client = MongoClient(Config.MONGO_URI)
 db = client['capstone-project']
-collection = db['images']
+images_collection = db['images']
 devices_collection = db['devices']
 
 def save_image_metadata(uuid_label, device_id, crop_path, whole_frame_path, single_box_path):
@@ -16,21 +15,16 @@ def save_image_metadata(uuid_label, device_id, crop_path, whole_frame_path, sing
         whole_frame_url = f"/static/{os.path.relpath(whole_frame_path, 'static')}"
         single_box_url = f"/static/{os.path.relpath(single_box_path, 'static')}"
 
-        collection.update_one(
-            {'_id': device_id},
-            {'$push': {
-                'image_list': {
-                    'uuid': uuid_label,
-                    'entry_timestamp': datetime.now().isoformat(),
-                    'exit_timestamp': None,
-                    'date': datetime.now().date().isoformat(),
-                    'crop_url': crop_url,
-                    'whole_frame_url': whole_frame_url,
-                    'single_box_url': single_box_url
-                }
-            }},
-            upsert=True
-        )
+        images_collection.insert_one({
+            'uuid': uuid_label,
+            'device_id': device_id,
+            'entry_timestamp': datetime.now().isoformat(),
+            'exit_timestamp': None,
+            'date': datetime.now().date().isoformat(),
+            'crop_url': crop_url,
+            'whole_frame_url': whole_frame_url,
+            'single_box_url': single_box_url
+        })
         logger.info(f"Saved URLs in MongoDB for person {uuid_label} with device ID {device_id}")
     except Exception as e:
         logger.error(f"Error saving URLs in MongoDB for person {uuid_label}: {e}")
@@ -38,24 +32,32 @@ def save_image_metadata(uuid_label, device_id, crop_path, whole_frame_path, sing
 def update_exit_timestamps(current_uuids, new_detected_uuids):
     try:
         for uuid_label in current_uuids - new_detected_uuids:
-            collection.update_one(
-                {'image_list.uuid': uuid_label},
-                {'$set': {'image_list.$.exit_timestamp': datetime.now().isoformat()}}
+            images_collection.update_one(
+                {'uuid': uuid_label},
+                {'$set': {'exit_timestamp': datetime.now().isoformat()}}
             )
             logger.info(f"Updated exit timestamp for person {uuid_label}")
     except Exception as e:
         logger.error(f"Error updating exit timestamps: {e}")
 
 def save_basic_image_metadata(uuid_label, device_id, cropped_frame_name, timestamp):
-    collection.insert_one({
-        "_id": uuid_label,
-        "device_id": device_id,
-        "file_name": cropped_frame_name,
-        "time": timestamp
-    })
+    try:
+        images_collection.insert_one({
+            'uuid': uuid_label,
+            'device_id': device_id,
+            'file_name': cropped_frame_name,
+            'time': timestamp
+        })
+        logger.info(f"Saved basic image metadata in MongoDB for person {uuid_label} with device ID {device_id}")
+    except Exception as e:
+        logger.error(f"Error saving basic image metadata in MongoDB for person {uuid_label}: {e}")
 
 def update_device_status(device_id, status):
-    devices_collection.update_one({"_id": device_id}, {"$set": {"status": status}})
+    try:
+        devices_collection.update_one({"_id": device_id}, {"$set": {"status": status}})
+        logger.info(f"Updated device status to {status} for device ID {device_id}")
+    except Exception as e:
+        logger.error(f"Error updating device status for device {device_id}: {e}")
 
 def get_rtsp_url(device_id):
     try:
